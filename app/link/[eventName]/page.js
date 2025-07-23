6dc1d49c67c66d8c43f130e5807d2c58a4420d21
Soh Hong Yu <sohhongyu@gmail.com>
2025-07-24 01:54:20 +0800

    Added AIMM Feedback Default Link

---
+++ app/link/[eventName]/page.js
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+const redirectMap = {
+  aimm: "https://forms.gle/hakB9A23qGEiYPYW6",
+};
+
+export default function Page({ params }) {
+  const { eventName } = params;
+  const router = useRouter();
+
+  useEffect(() => {
+    const key = eventName.toLowerCase();
+    const destination = redirectMap[key];
+
+    if (!destination) {
+      router.replace("/404");
+    } else if (destination.startsWith("http")) {
+      window.location.href = destination;
+    } else {
+      router.replace(destination);
+    }
+  }, [eventName, router]);
+
+  return (
+    <div className="h-[100svh] w-full flex items-center justify-center text-2xl">
+      Redirecting...
+    </div>
+  );
+}
